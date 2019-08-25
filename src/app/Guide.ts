db821e23ec5cb008c78054177aa5e1d764813cdf
Jonathan Garcia Rey <jgrrf1@gmail.com>
2019-08-25 17:37:27 -0500

    loading filters.

---
+++ src/app/Guide.ts
@@ -0,0 +1,11 @@
+export class Guide {
+    id: bigint;
+    name: string;
+    phrase: string;
+    photo: string;
+    facebook: string;
+    instagram: string;
+    phone: string;
+    email: string;
+    cityId: bigint;
+}
