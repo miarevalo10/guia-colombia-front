6f3cbf08695a67460ff103dfee75a13e379f45bf
Jonathan Garcia Rey <jgrrf1@gmail.com>
2019-09-08 11:07:49 -0500

    added header to city and category call

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {Category} from './Category';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../environments/environment';
+import {Globals} from './globals';
 
 @Injectable({
     providedIn: 'root'
@@ -12,12 +13,19 @@ export class CategoryService {
     CATEGORY_SERVICE = `${environment.url}/guiaturismo/category`;
     categoryList: Category[];
 
-    constructor(private httpClient: HttpClient) {
+    constructor(private httpClient: HttpClient,
+                private globals: Globals) {
     }
 
     getCategoryList(): Observable<Category[]> {
         this.categoryList = [];
-        this.httpClient.get(this.CATEGORY_SERVICE).subscribe((data: Array<any>) => {
+
+        const httpOptions = {
+            headers: new HttpHeaders({
+                Authorization: `Token ${this.globals.getToken()}`
+            })
+        };
+        this.httpClient.get(this.CATEGORY_SERVICE, httpOptions).subscribe((data: Array<any>) => {
             data.forEach(dataItem => {
                 const category = new Category();
                 category.id = dataItem.pk;
--- src/app/mail.service.ts
+++ src/app/mail.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { UtilsService } from './utils.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Globals } from './globals';
 import { environment } from 'src/environments/environment';
